a56325aa15cb50e3cf89f839114ea265f9a58d07
George G. Vega Yon <g.vegayon@gmail.com>
2022-08-29 12:05:11 -0600

    Updating the way in which the title shows

--- docs/functions.R
+++ docs/functions.R
@@ -44,14 +44,14 @@ menu_maker <- function(this = c("README.Rmd"), path = "", escape_bar = FALSE) {
   # Reaming to get the md
   names(yam) <- gsub("Rmd$", "md", names(yam))
   
-  res <- "\n [![Number of entries](https://img.shields.io/badge/dynamic/json?label=Entries&query=message&url=https%3A%2F%2Fut-iddynamics.github.io%2Fwepidemics%2Finfo%2Fentries.json)](https://github.com/UT-IDDynamics/wepidemics) [![Last update](https://img.shields.io/github/last-commit/UT-IDDynamics/wepidemics)](https://github.com/UT-IDDynamics/wepidemics)"
+  res <- "\n[![Number of entries](https://img.shields.io/badge/dynamic/json?label=Entries&query=message&url=https%3A%2F%2Fut-iddynamics.github.io%2Fwepidemics%2Finfo%2Fentries.json)](https://github.com/UT-IDDynamics/wepidemics) [![Last update](https://img.shields.io/github/last-commit/UT-IDDynamics/wepidemics)](https://github.com/UT-IDDynamics/wepidemics)\n\n"
   
   res <- paste0(res, Map(\(name., link.) {
     sprintf("[**%s**](%s%s)", name., path, link.)
   }, link. = names(yam), name. = sapply(yam, "[[", "title")) |>
     unlist() |> paste(collapse = ifelse(escape_bar, " \\| ", " | ")),  "\n")
   
-  res <- paste0(res, '<p style="font-size:10px;font-weight:bold;">(hosted at <a href="https://github.com/UT-IDDynamics/wepidemics" target="_blank">github.com/UT-IDDynamics</a>)</p>\n\n')
+  # res <- paste0(res, '<p style="font-size:10px;font-weight:bold;">(hosted at <a href="https://github.com/UT-IDDynamics/wepidemics" target="_blank">github.com/UT-IDDynamics</a>)</p>\n\n')
   res <- paste0(res, "---\n")
   
   cat(res)
